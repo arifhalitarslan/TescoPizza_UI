2638597cd96a3aef243a0dad4694b08c06632178
arifhalitarslan <97385377+arifhalitarslan@users.noreply.github.com>
2024-05-20 13:25:10 +0300

    first commit

---
+++ TescoPizza/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    {/* Fonksiyonlar buraya yazılacak */}
+  return (
+    <div>
+        
+        <p>navbar</p>
+
+    </div>
+  )
+}
+
+export default Navbar

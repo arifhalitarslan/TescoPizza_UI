ce3be746b62fbf1476cd36e8ad6ee3df18878d71
arifhalitarslan <97385377+arifhalitarslan@users.noreply.github.com>
2024-05-20 13:33:16 +0300

    routes added

--- TescoPizza/src/App.jsx
+++ TescoPizza/src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import Navbar from "./Components/Navbar";
 
 function App() {
-
-
   return (
     <>
-     
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
